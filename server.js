a5d1aac0633da60d0b0f82f0dd65c803fd4cac37
John Weber <johnhenryweber@gmail.com>
2019-11-13 12:34:19 -0800

    Heroku Fix?

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ function compareProfiles(allProfiles){
 app.get('/survey', function(req, res){
 
   // console.log(req.url);
-  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+  res.sendFile(path.join(__dirname, "/public/survey.html"));
 }
 );
 
@@ -73,15 +73,15 @@ app.get('/api/questions', function(req, res){
 });
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, "/app/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get('/results', function(req, res){
-  res.sendFile(path.join(__dirname, "/app/public/results.html"));
+  res.sendFile(path.join(__dirname, "/public/results.html"));
 });
 
 app.get('/allProfiles', function(req, res){
-  res.sendFile(path.join(__dirname, "/app/public/allProfiles.html"));
+  res.sendFile(path.join(__dirname, "/public/allProfiles.html"));
 });
 
 app.get('/api/results', function(req, res){
